2d41431077f2c6c45e954029eaab46bd9732b6ae
Bienvenu Murenzi <bienvenu@paytrippa.com>
2022-01-01 18:51:05 +0200

    Fix, Typescript deployment error

--- helpers/auth_helper.ts
+++ helpers/auth_helper.ts
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 
+const key = process.env.JWT_KEY;
 export const extract_token = (header:string) => {
     if(!header || !header.startsWith('Bearer')) throw new Error('Unauthorized');
     const token = header.split(' ')[1];
@@ -7,12 +8,12 @@ export const extract_token = (header:string) => {
     return token;
 }
 
-export const decode_token = (token:string) => {
-    const payload = jwt.verify(token, process.env.JWT_KEY);
+export const decode_token = (token:any) => {
+    const payload = jwt.verify(token, String(key));
     return payload;
 }
 
 export const encode_token = (payload:object) => {
-    const token = jwt.sign(payload, process.env.JWT_KEY, {expiresIn: '1d'});
+    const token = jwt.sign(payload, String(key), {expiresIn: '1d'});
     return token
 }
